2b3256552a40b0b6d0df3c906c220e1a9506c332
kgoudreau21 <kgoudreau21@ubishops.ca>
2025-10-29 12:27:08 -0400

    small changes to nav.js

--- my_site/nav.js
+++ my_site/nav.js
@@ -1,7 +1,10 @@
 function splitAtRoot(path){
     const url = new URL(path, location.origin);
     const pathFromRoot = url.pathname;
-    //console.log("---> path from root: " + pathFromRoot);
+    
+    console.log("splitAtRoot called for: " + path + "\n" 
+        + "Returns pathFromRoot: " + pathFromRoot);
+    
     return pathFromRoot;
 };
 
@@ -20,21 +23,34 @@ function setNav(current_path){
 
         //Following code will apply the "current_page" class to <a> in <nav> with (href === current_page)
         current_path = splitAtRoot(current_path);
+        console.log("current_path: " + current_path);
+
         let nav = document.getElementById("main-nav");
 
         for(let child of nav.children){
-            //console.log(splitAtRoot(child.href));
-            
+            let temp = splitAtRoot(child.href);
+
             if(child instanceof HTMLAnchorElement){
-                if(splitAtRoot(child.href) === current_path){
-                    //console.log("if condition passed!");
+                if(temp === current_path){
+                    console.log("if condition passed!");
                     child.classList.add("current_page");
+                    break;
                 }
                 
-                //check for a link finishing with a simple "/"" (for my site its "/CS203/"")
-                if(splitAtRoot(child.href) === "/CS203/index.php" && current_path === "/CS203/"){
-                    //console.log("if condition passed!");
+                //check for a link finishing with a simple "/"
+                //for my site hosted on: https://osiris.ubishops.ca/home/kgoudreau/
+                if(temp === "/home/kgoudreau/index.php" && current_path === "/home/kgoudreau/"){
+                    console.log("if condition passed!");
                     child.classList.add("current_page");
+                    break;
+                }
+
+                //check for a link finishing with a simple "/"
+                //for my github site on: kgoudreau21.github.io/CS203/
+                if(temp === "/CS203/index.php" && current_path === "/CS203/"){
+                    console.log("if condition passed!");
+                    child.classList.add("current_page");
+                    break;
                 }
             }
         }
